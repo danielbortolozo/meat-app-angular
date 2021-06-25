df9c9b9667f98a84a492a19da3c247fadab563d4
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2021-06-25 01:12:20 -0300

    Implementando componente de frete

---
+++ src/app/order/ordem-items/ordem-items.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
+
+@Component({
+  selector: 'mt-ordem-items',
+  templateUrl: './ordem-items.component.html'
+})
+export class OrdemItemsComponent implements OnInit {
+
+
+ @Input() items: CartItem[];
+
+ @Output() increaseQty = new EventEmitter<CartItem>()
+ @Output() decreaseQty = new EventEmitter<CartItem>()
+ @Output() remove = new EventEmitter<CartItem>()
+ 
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  emitIncreaseQty(item: CartItem) {
+    this.increaseQty.emit(item)
+  }
+
+  emitDecreaseQty(item: CartItem) {
+    this.decreaseQty.emit(item)
+  }
+
+  emitRemove(item: CartItem) {
+    this.remove.emit(item)
+  }
+
+}
---
+++ src/app/order/order.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
+import { RadioOption } from 'app/shared/radio/radio-option.model';
+import { OrderService } from './order.service';
+
+@Component({
+  selector: 'mt-order',
+  templateUrl: './order.component.html'
+})
+export class OrderComponent implements OnInit {
+
+
+  paymentOptions: RadioOption[] = [
+    {label: 'Dinheiro', value: 'MONEY'},
+    {label: 'Cartao Credito', value: 'CARD_CREDITO'},
+    {label: 'Cartao Debito', value: 'CARD_DEBITO'}
+  ]
+
+
+  constructor(private orderService: OrderService) { }
+
+  ngOnInit() {
+  }
+
+
+  cartItems(): CartItem[] {
+    return this.orderService.cartItems()
+  }
+
+  increaseQty(item: CartItem) {
+     this.orderService.increaseQty(item);
+  }
+  decreaseQty(item: CartItem) {
+    this.orderService.decreaseQty(item);
+  }
+
+  remove(item: CartItem) {
+
+    this.orderService.removeItem(item)
+  }
+
+}
